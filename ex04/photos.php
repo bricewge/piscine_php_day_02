fdd91b54024bf18b724567e101264db9275d873c
Brice Waegeneire <brice.wge@gmail.com>
2017-06-15 13:36:30 +0200

    ex04 working

--- ex04/photos.php
+++ ex04/photos.php
@@ -2,27 +2,37 @@
 <?PHP
 
 if ($argc > 1) {
-    $ch = curl_init($argv[1]);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $page = curl_exec($ch);
-    // $print_r($page);
-    print_r(preg_match_all("|<img src=\"(.*?)\".*?>|", $page, $imgurls));
-    print_r($imgurls);
-    foreach($imgurls[1] as $imgurl) {
-        if (print_r(preg_match("|^https?://|") == 0)) {
-            $imgurl = $argv[1].$imgurl;
+    // Get the page
+    if (($ch = curl_init($argv[1])) !== FALSE) {
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $page = curl_exec($ch);
+        curl_close($ch);
+    }
+    // Get urls of the images
+    if ($page !== NULL) {
+        preg_match_all("|<img.*?src=\"(.*?)\".*?>|", $page, $imgurls);
+        foreach($imgurls[1] as $i => $imgurl) {
+            if (preg_match("|^https?://|", $imgurl) === 0) {
+                $imgurls[1][$i] = $argv[1].$imgurl;
+            }
         }
     }
-    print_r($imgurls);
-    $img = curl_init($imgurl);
-    curl_setopt($img, CURLOPT_RETURNTRANSFER, 1);
-    $page = curl_exec($ch);
-    // if ($ch != FALSE) {
-    //     preg_match("|^https?://(.*)$|", $argv[1], $dirname);
-    //     // echo $dirname[1];
-    //     if (mkdir($dirname[1], "755")) {
-    //         // echo "Made it!\n";
-    //     }
-    // }
+    // Make dir
+    if ($imgurls[1] != NULL) {
+        $dirname = parse_url($argv[1], PHP_URL_HOST);
+        mkdir($dirname, 0755);
+        // Download images
+        foreach ($imgurls[1] as $imgurl) {
+            $ch_img = curl_init($imgurl);
+            curl_setopt($ch_img, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch_img, CURLOPT_FOLLOWLOCATION, 1);
+            $img = curl_exec($ch_img);
+            curl_close($img);
+            $filename = pathinfo($imgurl);
+            $fd = fopen($dirname."/".$filename["basename"], "w");
+            fputs($fd, $img);
+            fclose($fd);
+        }
+    }
 }
 ?>
